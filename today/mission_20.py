6c97b838e7b43a6f590ff4596e374e31f562c702
Ayame <aym00124@g.yju.ac.kr>
2024-04-17 20:43:25 +0900

    단어를 카운트 주석추가

--- today/mission_20.py
+++ today/mission_20.py
@@ -4,18 +4,25 @@ msg = input("문자열 입력:") #.split()
 # 사용자 지정 단어 입력받기
 input_msg = input("단어 입력:")
 
+# word 선언
 word = ""
 # 카운트 선언
 count = 0
 
+# msg를 반복
 for char in msg :
+    # 공백 아니면 word에 추가
     if char != " ":
         word += char
+    # 아니면 
     else:
+        # word == 지정 단어면 count +1
         if input_msg == word:
             count += 1
-        word = ""    
-            
+        # word 초기화 
+        word = ""   
+         
+# 마지막 word 검사 -> word == 지정 단어면 count +1           
 if input_msg == word:
             count += 1
         
