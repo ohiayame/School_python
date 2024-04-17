ae0dbb3c2f7ab11944bde22c02fe6d6ba5dd6d07
Ayame <aym00124@g.yju.ac.kr>
2024-04-17 17:29:49 +0900

    append 使わない

--- aa/lesson1.py
+++ aa/lesson1.py
@@ -1,1 +1,34 @@
-   
+input_msg = input("단어 입력:")
+
+# 카운트 선언
+count = 0
+
+# 단어의 시작 위치 선언
+word_start = 0
+
+# 입력된 메시지의 길이
+length = len(input_msg)
+
+# 입력된 메시지를 반복
+while word_start < length:
+    # 공백이 아닌 문자의 위치 찾기
+    while word_start < length and input_msg[word_start] == " ":
+        word_start += 1
+    
+    # 단어의 끝 위치 찾기
+    word_end = word_start
+    while word_end < length and input_msg[word_end] != " ":
+        word_end += 1
+    
+    # 추출된 단어
+    word = input_msg[word_start:word_end]
+    
+    # 추출된 단어와 입력한 단어가 일치하는 경우 카운트 증가
+    if word == input_msg:
+        count += 1
+    
+    # 다음 단어의 시작 위치 설정
+    word_start = word_end + 1
+
+# 카운트 결과 출력
+print("단어 {}의 출현 빈도:".format(input_msg), count)
--- today/mission_20.py
+++ today/mission_20.py
@@ -1,16 +1,23 @@
 # 문자열 입력받기 split
-msg = input("문자열 입력:").split()
+msg = input("문자열 입력:") #.split()       
+        
 # 사용자 지정 단어 입력받기
 input_msg = input("단어 입력:")
 
+word = ""
 # 카운트 선언
 count = 0
 
-# 문자열를 반복
-for message in msg :
-# 지정 단어가 나오면 카운트
-    if input_msg == message:
-        count += 1
+for char in msg :
+    if char != " ":
+        word += char
+    else:
+        if input_msg == word:
+            count += 1
+        word = ""    
+            
+if input_msg == word:
+            count += 1
         
 # 카운트 결과를 출력
 print("단어{}의 출현 빈도:".format(input_msg), count)
