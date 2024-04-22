70c143a3a12ac389304f8c80376e77606c359944
Ayame <0819aym001@gmail.com>
2024-04-22 19:56:46 +0900

    구구단 최종

--- aa/1_기출문제/1_모의문제_교수님.py
+++ aa/1_기출문제/1_모의문제_교수님.py
@@ -10,14 +10,16 @@ while True:
     
     # 메뉴값 == 1
     if select_menu == '1':
-        print("구구단 출력, 추후 구현")
-        
-        # 구구단의 단 입력
-        dan = int(input())
-        #해당 단위 구구단
-        for num in range(1,10):
-            print(dan,"X",num, "=", dan * num)
-            
+        while True:
+        #print("구구단 출력, 추후 구현")
+
+            # 구구단의 단 입력
+            dan = int(input("단을 입력 하세요: "))
+            if 2 <= dan <= 9:
+                #해당 단위 구구단
+                for num in range(1,10):
+                    print(dan, "X", num, "=", dan * num)
+                break
     # 메뉴 값 == 2
     elif select_menu =='2':
         # 프로그램 종료
@@ -28,3 +30,12 @@ while True:
     # 에러메시지 출력
     else:
         print("메뉴 값은 1 또는 2만 입력")            
+        
+        
+        
+        
+# 1~100 사이 정수중 3의 배수와 7의 배수를 출력하시오        
+for num in range(1,101):
+    if num % 3 == 0 or num % 7 == 0:
+        print(num,"\t",end='')
+    
