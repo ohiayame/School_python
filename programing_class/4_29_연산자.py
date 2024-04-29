5b2c9484ff8790e7a942b9ad875648ca5f7dd6b3
Ayame <0819aym001@gmail.com>
2024-04-29 19:22:14 +0900

    자료형 형반환

---
+++ programing_class/4_29_연산자.py
@@ -0,0 +1,11 @@
+# 더하기
+value1 = 1 + 2
+# 빼기
+value2 = 3 - 1.5
+# 곱하기
+value3 = 3 * 2
+# 나누기
+value4 = 4 / 2
+# 출력
+print(value1, value2, value3, value4)
+print(type(value1),type(value2),type(value3),type(value4))
