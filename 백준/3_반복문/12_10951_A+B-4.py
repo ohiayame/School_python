b64ac0d33368b637faf57e554125622999123e8c
Ayame <0819aym001@gmail.com>
2024-05-02 14:19:20 +0900

    3단계

---
+++ 백준/3_반복문/12_10951_A+B-4.py
@@ -0,0 +1,18 @@
+import sys
+
+li = []
+
+while True:
+    try:       
+        a, b = map (int, sys.stdin.readline().split())
+        
+        if a == 0 and b == 0:
+            break
+        else:
+            li.append(a + b)
+        
+    except:
+        break
+    
+for sum in li:
+    print(sum)
---
+++ 백준/3_반복문/9_2438_별찍기-1.py
@@ -0,0 +1,3 @@
+num = int(input())
+for value in range(1,num+1):
+    print("*" * value)
