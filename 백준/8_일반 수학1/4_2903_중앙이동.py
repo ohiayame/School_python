d865221177a3d7d9f7cee824992d2845a79dcaef
Ayame <0819aym001@gmail.com>
2024-06-08 01:23:55 +0900

    yarinaosi

--- 백준/8_일반 수학1/4_2903_중앙이동.py
+++ 백준/8_일반 수학1/4_2903_중앙이동.py
@@ -1,13 +1,9 @@
 input_num = int(input())
 num = 3
+tohagi = 1
+for _ in range(input_num - 1):
+    tohagi *= 2
+    num += tohagi
 
-if input_num == 1:
-    num = 3 ** 2
-else:
-    tohagi = 1
-    for _ in range(input_num - 1):
-        tohagi *= 2
-        num += tohagi
-    
 print(num ** 2)
 print(num)
