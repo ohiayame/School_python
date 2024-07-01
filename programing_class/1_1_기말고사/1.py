62421f98c1f683a518ca731774c0dca9e8bd8c1f
Ayame <166793391+ohiayame@users.noreply.github.com>
2024-07-01 20:27:37 +0900

    기말 1번 문제

--- programing_class/1_1_기말고사/1.py
+++ programing_class/1_1_기말고사/1.py
@@ -1,8 +1,8 @@
 import random
 # 사용자로부터 입력 받기
 while True:
-    n = int(input("Enter the number of dice rolls (between 100 and 1,000,000): "))
-    if 100 <= n <= 1000000:
+    input_n = int(input("Enter the number of dice rolls (between 100 and 1,000,000): "))
+    if 100 <= input_n <= 1000000:
         break
     print("Please enter a number within the specified range.")
     
@@ -10,7 +10,7 @@ while True:
 dice_li = [0,0,0,0,0,0]
 
 # n만큼 주사위를 던지기
-for i in range(n):
+for i in range(input_n):
     dice_num = random.randint(1,6)
     dice_li[dice_num-1] += 1
 
@@ -23,15 +23,14 @@ for num in dice_li:
 # 별 개수 계산
 stars_li = [0,0,0,0,0,0]
 index = 0
-h = 0
+
 for n in dice_li:
     stars = (n / max_num) * 10
     stars_li[index] = (int(stars))
     index += 1
-    # 확률
-    h += n
+
 
 # 결과 출력
 print("\nDice Roll Frequency Histogram:")
 for i in range(6):
-    print(f"{i + 1}: {stars_li[i] * "*"} ({dice_li[i]} times, {((dice_li[i]/h)*100)}%)")
+    print(f"{i + 1}: {stars_li[i] * "*"} ({dice_li[i]} times, {((dice_li[i]/input_n)*100)}%)")
