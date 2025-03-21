eb8d141d72e16f5712b0998196640df1b3b11a9f
Ayame <166793391+ohiayame@users.noreply.github.com>
2025-03-21 22:10:25 +0900

    경사하강법

--- 기계/0319_test.py
+++ 기계/0319_test.py
@@ -10,13 +10,35 @@ y_train = [ val + np.random.rand()* 5 for val in x_train]
 
 # BGB (Batch Gradient Descent)
 # 배치 경사 하강법을 이용하여 Linear regression적용
+w = 0.0
+leaning_rate = 0.01
+epoch = 50
+w_list = []
+for num_of_epoch in range(epoch):
+    gradient_sum = 0.0
+    
+    # GD 수행 후 최저의 W 값 도출
+    for x, y in zip(x_train, y_train):
+        # predict_value
+        # 기울기 값
+        gradient_sum += x * (w * x - y)
+        
+    # W값 업데이트
+    print(gradient_sum)
+    w = w - leaning_rate * (gradient_sum / len(x_train))
+    
+    w_list.append(w)
 
-print(x_train)
-print("-" *100)
-print(y_train)
+x_test = [val for val in range(10)]
+y_test = [w * val for val in x_test]
 
+# print(x_train)
+# print("-" *100)
+# print(y_train)
+print(w_list)
 
 plt.scatter(x_train, y_train, color = "blue")
+plt.plot(x_test, y_test)
 plt.show()
 #output
 #label
